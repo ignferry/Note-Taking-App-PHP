deec56423dcc20df5f3b0e064f6fd8c902e9cf89
Ignasius Ferry <ignasius.ferry01@gmail.com>
2022-09-18 00:22:14 +0700

    Structuring project

---
+++ App/App.php
@@ -0,0 +1,15 @@
+<?php
+
+class App {
+    private $router;
+
+    public function __construct() {
+        
+    }
+
+    private function parseURL() {
+        
+    }
+}
+
+?>
---
+++ App/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+class Router {
+    private $routes = [];
+
+    function __construct() {
+        // Autoload from routes folder
+
+    }
+
+    function handleRequest($method, $path) {
+        // Iterates through routes to find a handler for the corresponding route
+        
+    }
+}
+
+?>
