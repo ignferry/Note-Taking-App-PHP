1aed53e9cfb68edcfd2b867ecea54d4592676b15
Ignasius Ferry <ignasius.ferry01@gmail.com>
2022-09-27 01:50:13 +0700

    note edit page

--- App/views/notes/edit.php
+++ App/views/notes/edit.php
@@ -1,5 +1,30 @@
 <?php require_once "views/components/header.php"?>
 
-<h1>Note editing page</h1>
+<form class="edit-form" action="/notes/<?php echo $data["note"]["id"]?>" method="post">
+    <div id="notes-toolbar">
+        <a href="/notes" class="icon-button">
+            <img src="/public/images/back.svg">
+        </a>
+        <button type="submit" name="_method" class="medium-button" value="PUT">Update note</button>
+        <button type="submit" name="_method" class="medium-button" value="DELETE">Delete note</button>
+
+    </div>
+
+    <hr>
+    
+    <div id="note-title-div">
+        <input type="text" class= "title-field" name="title" value="<?php echo $data["note"]["title"] ?>" placeholder="Title">
+    </div>
+
+    <hr>
+
+    <div class="note-content-div">
+        <textarea class="content-field" name="content" oninput='this.style.height = "";this.style.height = (this.scrollHeight) + "px"'><?php echo $data["note"]["content"] ?></textarea>
+    </div>
+</form>
+
+
+
+
 
 <?php require_once "views/components/footer.php"?>
