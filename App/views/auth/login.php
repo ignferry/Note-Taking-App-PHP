e460713917cfef775310eddade21b95576fdb2c3
johannes-ws <johannessukiatmodjo20@gmail.com>
2022-09-26 17:13:36 +0700

    login & register (html files)

--- App/views/auth/login.php
+++ App/views/auth/login.php
@@ -1,1 +1,21 @@
-<h1>Login page</h1>
+<?php require_once "views/components/header.php"?>
+<div class="login-page">
+    <form>
+        <h2>Login</h2>
+        <div class="content">
+            <div class="input-field">
+                <input type="email" id="email" name="email" placeholder="Email Address" required>
+            </div>
+            <div class="input-field">
+                <input type="password" id="password" name="password" placeholder="Password" required>
+            </div>
+        </div>
+        <div class="action">
+            <button>Login</button>
+        </div>
+        <div class="create-account">
+            <p>Not a member? <a href="/register">Create Account</a><p>
+        </div>
+    </form>
+</div>
+<?php require_once "views/components/footer.php"?>
--- App/views/auth/register.php
+++ App/views/auth/register.php
@@ -1,2 +1,24 @@
-<h1>Registration page</h1>
-<?php echo isset($_SESSION["error"]) ? $_SESSION["error"] : "No Error"?>
+<?php require_once "views/components/header.php"?>
+<div class="registration-page">
+    <form>
+        <h2>Sign Up</h2>
+        <div class="content">
+            <div class="input-field">
+                <input type="name" id="name" name="name" placeholder="Name" required>
+            </div>
+            <div class="input-field">
+                <input type="email" id="email" name="email" placeholder="Email Address" required>
+            </div>
+            <div class="input-field">
+                <input type="password" id="password" name="password" placeholder="Password" required>
+            </div>
+        </div>
+        <div class="action">
+            <button>Create Account</button>
+        </div>
+        <div class="sign-in">
+            <p>Already have an account? <a href="/login">Sign in</a><p>
+        </div>
+    </form>
+</div>
+<?php require_once "views/components/footer.php"?>
