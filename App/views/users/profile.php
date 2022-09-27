ad168df441a91175efcb9ad1937d8111c2ddf513
Ignasius Ferry <ignasius.ferry01@gmail.com>
2022-09-27 08:39:12 +0700

    Profile

--- App/views/users/profile.php
+++ App/views/users/profile.php
@@ -1,1 +1,52 @@
-<h1>Profile page</h1>
+<?php require_once "views/components/header.php"?>
+
+<div id="profile-div">
+    <h1>Profile</h1>
+    <hr>
+
+    <form action="/changePassword" method="post">
+        <div id="profile-form-div">
+        
+            <input hidden name="_method" value="PUT">
+            <table class="profile-table">
+                <tr>
+                    <th>Name</th>
+                    <td><?php echo $data["user"]["name"] ?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?php echo $data["user"]["email"] ?></td>
+                </tr>
+            </table>
+
+            <h2>Change Password</h2>
+            <hr>
+            <table class="profile-table">
+                <tr>
+                    <th>Current Password</th>
+                    <td>
+                        <input type="password" name="currentPassword" required>
+                    </td>
+                </tr>
+                <tr>
+                    <th>New Password</th>
+                    <td>
+                        <input type="password" name="newPassword" required>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Confirm New Password</th>
+                    <td>
+                        <input type="password" name="newPasswordConfirm" required>
+                    </td>
+                </tr>
+            </table>
+
+            <div class="center">
+                <button type="submit" class="medium-button">Confirm</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php require_once "views/components/footer.php"?>
